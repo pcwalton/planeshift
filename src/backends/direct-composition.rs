be6de3952886c260fa90e293b4facddcab19be9e
Patrick Walton <pcwalton@mimiga.net>
2018-11-05 13:10:36 -0800

    Implement screenshots in the DirectComposition backend.
    This is a pretty clunky implementation, relying on sending the Print
    Screen key and capturing the results. But that's the only semi-reliable
    method I could find...

--- src/backends/direct-composition.rs
+++ src/backends/direct-composition.rs
@@ -1,26 +1,38 @@
 // planeshift/src/backends/direct-composition.rs
 
 use euclid::Rect;
+use image::{ConvertBuffer, RgbaImage};
 use mozangle::egl::ffi::types::{EGLClientBuffer, EGLConfig, EGLContext, EGLDisplay, EGLSurface};
 use mozangle::egl::ffi::{D3D11_DEVICE_ANGLE, EGLDeviceEXT};
 use mozangle::egl;
+use std::cell::RefCell;
 use std::ffi::c_void;
+use std::mem;
 use std::ptr;
+use std::slice;
+use std::sync::mpsc::{self, Sender};
+use std::thread::Builder as ThreadBuilder;
 use winapi::Interface;
 use winapi::shared::dxgi1_2::{DXGI_ALPHA_MODE_PREMULTIPLIED, DXGI_SCALING_STRETCH};
 use winapi::shared::dxgi1_2::{DXGI_SWAP_CHAIN_DESC1, IDXGIFactory2, IDXGISwapChain1};
 use winapi::shared::dxgi::{DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL, IDXGIAdapter, IDXGIDevice};
 use winapi::shared::dxgiformat::DXGI_FORMAT_B8G8R8A8_UNORM;
 use winapi::shared::dxgitype::{DXGI_SAMPLE_DESC, DXGI_USAGE_RENDER_TARGET_OUTPUT};
-use winapi::shared::minwindef::{FALSE, TRUE};
-use winapi::shared::windef::HWND;
+use winapi::shared::minwindef::{DWORD, FALSE, LPARAM, LRESULT, TRUE, UINT, WORD, WPARAM};
+use winapi::shared::ntdef::LPCSTR;
+use winapi::shared::windef::{HBRUSH, HWND, RECT};
 use winapi::shared::winerror::{self, S_OK};
 use winapi::um::d3d11::{self, D3D11_CREATE_DEVICE_BGRA_SUPPORT, D3D11_SDK_VERSION, ID3D11Device};
 use winapi::um::d3d11::{ID3D11Texture2D};
 use winapi::um::d3dcommon::{D3D_DRIVER_TYPE_HARDWARE, D3D_DRIVER_TYPE_WARP};
 use winapi::um::d3dcommon::{D3D_FEATURE_LEVEL_10_1};
 use winapi::um::dcomp::{self, IDCompositionDevice, IDCompositionTarget, IDCompositionVisual};
+use winapi::um::handleapi;
+use winapi::um::libloaderapi;
 use winapi::um::unknwnbase::IUnknown;
+use winapi::um::winbase;
+use winapi::um::wingdi::BITMAPINFOHEADER;
+use winapi::um::winuser::{self, INPUT, KEYBDINPUT, MSG, WNDCLASSEXA};
 
 #[cfg(feature = "enable-winit")]
 use winit::Window;
@@ -28,7 +40,8 @@ use winit::Window;
 use winit::os::windows::WindowExt;
 
 use crate::{Connection, ConnectionError, GLAPI, GLContextLayerBinding, LayerContainerInfo};
-use crate::{LayerGeometryInfo, LayerId, LayerMap, LayerSurfaceInfo, LayerTreeInfo, SurfaceOptions};
+use crate::{LayerGeometryInfo, LayerId, LayerMap, LayerSurfaceInfo, LayerTreeInfo, Promise};
+use crate::{SurfaceOptions};
 use self::com::ComPtr;
 
 pub struct Backend {
@@ -40,6 +53,8 @@ pub struct Backend {
 
     egl_device: EGLDeviceEXT,
     egl_display: EGLDisplay,
+
+    screenshot_window: Option<HWND>,
 
     #[cfg(feature = "enable-winit")]
     window: Option<Window>,
@@ -114,6 +129,8 @@ impl crate::Backend for Backend {
 
                 egl_device,
                 egl_display,
+
+                screenshot_window: None,
 
                 #[cfg(feature = "enable-winit")]
                 window,
@@ -206,6 +223,7 @@ impl crate::Backend for Backend {
     fn begin_transaction(&self) {}
 
     fn end_transaction(&mut self,
+                       promise: &Promise<()>,
                        _: &LayerMap<LayerTreeInfo>,
                        _: &LayerMap<LayerContainerInfo>,
                        _: &LayerMap<LayerGeometryInfo>,
@@ -213,6 +231,9 @@ impl crate::Backend for Backend {
         unsafe {
             let result = (**self.dcomp_device).Commit();
             assert_eq!(result, S_OK);
+
+            // FIXME(pcwalton): Is this right?
+            promise.resolve(());
         }
     }
 
@@ -288,7 +309,10 @@ impl crate::Backend for Backend {
 
         (**target).SetRoot(*native_info.visual);
 
-        native_info.target = Some(target);
+        native_info.target = Some(Target {
+            directcomposition_target: target,
+            window: host,
+        });
     }
 
     fn unhost_layer(&mut self, layer: LayerId) {
@@ -423,6 +447,87 @@ impl crate::Backend for Backend {
         }
     }
 
+    // Screenshots
+
+    fn screenshot_hosted_layer(&mut self,
+                               layer: LayerId,
+                               transaction_promise: &Promise<()>,
+                               _: &LayerMap<LayerTreeInfo>,
+                               _: &LayerMap<LayerContainerInfo>,
+                               _: &LayerMap<LayerGeometryInfo>,
+                               _: &LayerMap<LayerSurfaceInfo>)
+                               -> Promise<RgbaImage> {
+        self.create_screenshot_window_if_necessary();
+
+        let screenshot_window = self.screenshot_window.unwrap();
+
+        let window: HWND = self.native_component[layer].target.as_ref().unwrap().window;
+        let mut window_rect = RECT { left: 0, right: 0, top: 0, bottom: 0, };
+        unsafe {
+            assert_ne!(winuser::GetWindowRect(window, &mut window_rect), FALSE);
+
+            // The rectangle returned by `GetWindowRect` includes window decorations. Remove them.
+            let mut adjusted_rect = RECT { left: 0, right: 0, top: 0, bottom: 0, };
+            let style = winuser::GetWindowLongA(window, winuser::GWL_STYLE) as DWORD;
+            let ex_style = winuser::GetWindowLongA(window, winuser::GWL_EXSTYLE) as DWORD;
+            let has_menu = if winuser::GetMenu(window).is_null() { FALSE } else { TRUE };
+            winuser::AdjustWindowRectEx(&mut adjusted_rect, style, has_menu, ex_style);
+
+            window_rect = RECT {
+                left: window_rect.left - adjusted_rect.left,
+                right: window_rect.right - adjusted_rect.right,
+                top: window_rect.top - adjusted_rect.top,
+                bottom: window_rect.bottom - adjusted_rect.bottom,
+            }
+        }
+
+        let result_promise = Promise::new();
+        let request = RefCell::new(Some(Box::new(ScreenshotRequest {
+            promise: result_promise.clone(),
+            window_rect,
+        })));
+
+        transaction_promise.then(Box::new(move |()| {
+            unsafe {
+                // Try to bring the window to the front. This is best-effort.
+                winuser::SetForegroundWindow(window);
+
+                // Wake up our screenshot thread.
+                let request: Box<ScreenshotRequest> = request.replace(None).unwrap();
+                let request_addr = &*request as *const _ as WPARAM;
+                mem::forget(request);
+                winuser::PostMessageA(screenshot_window, winuser::WM_USER, request_addr, 0);
+
+                // Send a Print Screen key to capture the desktop.
+                let mut inputs = [
+                    INPUT { type_: winuser::INPUT_KEYBOARD, u: mem::zeroed(), },
+                    INPUT { type_: winuser::INPUT_KEYBOARD, u: mem::zeroed(), },
+                ];
+                *inputs[0].u.ki_mut() = KEYBDINPUT {
+                    wVk: winuser::VK_SNAPSHOT as WORD,
+                    wScan: 0,
+                    dwFlags: 0,
+                    time: 0,
+                    dwExtraInfo: 0,
+                };
+                *inputs[1].u.ki_mut() = KEYBDINPUT {
+                    wVk: winuser::VK_SNAPSHOT as WORD,
+                    wScan: 0,
+                    dwFlags: winuser::KEYEVENTF_KEYUP,
+                    time: 0,
+                    dwExtraInfo: 0,
+                };
+
+                let events_sent = winuser::SendInput(inputs.len() as UINT,
+                                                     inputs.as_mut_ptr(),
+                                                     mem::size_of::<INPUT>() as _);
+                assert_eq!(events_sent, inputs.len() as UINT);
+            }
+        }));
+
+        result_promise
+    }
+
     // `winit` integration
 
     #[cfg(feature = "enable-winit")]
@@ -448,6 +553,20 @@ impl crate::Backend for Backend {
     }
 }
 
+impl Backend {
+    fn create_screenshot_window_if_necessary(&mut self) {
+        if self.screenshot_window.is_some() {
+            return
+        }
+
+        let (window_sender, window_receiver) = mpsc::channel();
+        ThreadBuilder::new().name("PlaneshiftScreenshotThread".to_string()).spawn(move || {
+            screenshot_thread(window_sender)
+        }).unwrap();
+        self.screenshot_window = Some(window_receiver.recv().unwrap().0);
+    }
+}
+
 impl Drop for Backend {
     fn drop(&mut self) {
         unsafe {
@@ -458,8 +577,14 @@ impl Drop for Backend {
 
 struct NativeInfo {
     visual: ComPtr<IDCompositionVisual>,
-    target: Option<ComPtr<IDCompositionTarget>>,
+    target: Option<Target>,
     surface: Option<Surface>,
+}
+
+struct Target {
+    #[allow(dead_code)]
+    directcomposition_target: ComPtr<IDCompositionTarget>,
+    window: HWND,
 }
 
 struct Surface {
@@ -489,6 +614,15 @@ impl Drop for GLContext {
 type MaybeWindow = ();
 #[cfg(feature = "enable-winit")]
 type MaybeWindow = Window;
+
+struct NativeWindow(HWND);
+
+unsafe impl Send for NativeWindow {}
+
+struct ScreenshotRequest {
+    promise: Promise<RgbaImage>,
+    window_rect: RECT,
+}
 
 fn unpack_connection(connection: Connection<*mut ID3D11Device>)
                      -> (*mut ID3D11Device, Option<MaybeWindow>) {
@@ -535,6 +669,125 @@ fn unpack_connection(connection: Connection<*mut ID3D11Device>)
                 (d3d_device.copy(), Some(window))
             }
         }
+    }
+}
+
+fn screenshot_thread(window_sender: Sender<NativeWindow>) {
+    static WINDOW_CLASS_NAME: &[u8] = b"PlaneshiftScreenshotWindow\0";
+
+    unsafe {
+        let hinstance = libloaderapi::GetModuleHandleA(ptr::null_mut());
+        let mut class = WNDCLASSEXA {
+            cbSize: mem::size_of::<WNDCLASSEXA>() as UINT,
+            style: 0,
+            lpfnWndProc: Some(screenshot_window_proc),
+            cbClsExtra: 0,
+            cbWndExtra: 0,
+            hInstance: hinstance,
+            hIcon: ptr::null_mut(),
+            hCursor: ptr::null_mut(),
+            hbrBackground: winuser::COLOR_WINDOW as HBRUSH,
+            lpszMenuName: ptr::null_mut(),
+            lpszClassName: WINDOW_CLASS_NAME.as_ptr() as LPCSTR,
+            hIconSm: ptr::null_mut(),
+        };
+        let class = winuser::RegisterClassExA(&mut class);
+        let window = winuser::CreateWindowExA(
+            winuser::WS_EX_OVERLAPPEDWINDOW,
+            class as LPCSTR,
+            WINDOW_CLASS_NAME.as_ptr() as LPCSTR,
+            0,
+            0,
+            0,
+            0,
+            0,
+            winuser::HWND_MESSAGE,
+            ptr::null_mut(),
+            hinstance,
+            ptr::null_mut());
+        assert_ne!(winuser::AddClipboardFormatListener(window), FALSE);
+        window_sender.send(NativeWindow(window)).unwrap();
+
+        let mut msg: MSG = mem::zeroed();
+        while winuser::GetMessageA(&mut msg, ptr::null_mut(), 0, 0) != 0 {
+            winuser::TranslateMessage(&mut msg);
+            winuser::DispatchMessageA(&mut msg);
+        }
+    }
+}
+
+unsafe extern "system" fn screenshot_window_proc(window: HWND,
+                                                 msg: UINT,
+                                                 wparam: WPARAM,
+                                                 lparam: LPARAM)
+                                                 -> LRESULT {
+    match msg {
+        winuser::WM_USER => {
+            winuser::SetWindowLongPtrA(window, winuser::GWLP_USERDATA, wparam as isize)
+        }
+
+        winuser::WM_CLIPBOARDUPDATE => {
+            let promise = winuser::GetWindowLongPtrA(window, winuser::GWLP_USERDATA) as
+                *mut ScreenshotRequest;
+            if promise.is_null() {
+                return winuser::DefWindowProcA(window, msg, wparam, lparam);
+            }
+
+            let request: Box<ScreenshotRequest> = mem::transmute(promise);
+            winuser::SetWindowLongPtrA(window, winuser::GWLP_USERDATA, 0);
+
+            assert_ne!(winuser::OpenClipboard(ptr::null_mut()), FALSE);
+
+            // Screenshot data should have no owner. Verify that.
+            //
+            // FIXME(pcwalton): This is still fragile, because other apps can also place ownerless
+            // data on the clipboard, so we might think we have screenshot data when it's actually
+            // some other app placing stuff on the clipboard. But this is better than nothing.
+            let owner = winuser::GetClipboardOwner();
+            if !owner.is_null() {
+                return winuser::DefWindowProcA(window, msg, wparam, lparam);
+            }
+
+            let mut clipboard = winuser::GetClipboardData(winuser::CF_DIB);
+            if clipboard == handleapi::INVALID_HANDLE_VALUE {
+                clipboard = winuser::GetClipboardData(winuser::CF_DIBV5);
+            }
+            if clipboard == handleapi::INVALID_HANDLE_VALUE {
+                return winuser::DefWindowProcA(window, msg, wparam, lparam);
+            }
+
+            let dib = winbase::GlobalLock(clipboard) as *mut BITMAPINFOHEADER;
+            assert!(!dib.is_null());
+
+            // Bitmap data is bottom-to-top, BGRA. Change to top-to-bottom, RGBA.
+            let src_data = slice::from_raw_parts(dib.offset(1) as *const u32,
+                                                 ((*dib).biSizeImage / 4) as usize);
+            let mut dest_data = Vec::with_capacity(src_data.len() * 4);
+            let screen_width = (*dib).biWidth as usize;
+            let screen_height = (*dib).biHeight as usize;
+            let rect = request.window_rect;
+            for y in (rect.top as usize)..(rect.bottom as usize) {
+                for x in (rect.left as usize)..(rect.right as usize) {
+                    let src_pixel = src_data[(screen_height - y - 1) * screen_width + x];
+                    dest_data.extend_from_slice(&[
+                        ((src_pixel >> 16) & 0xff) as u8,
+                        ((src_pixel >> 8)  & 0xff) as u8,
+                        ((src_pixel >> 0)  & 0xff) as u8,
+                        ((src_pixel >> 24) & 0xff) as u8,
+                    ]);
+                }
+            }
+
+            winbase::GlobalUnlock(dib as *mut _);
+
+            let image = RgbaImage::from_vec((rect.right - rect.left) as u32,
+                                            (rect.bottom - rect.top) as u32,
+                                            dest_data).unwrap().convert();
+            request.promise.resolve(image);
+            0
+        }
+
+        _ => winuser::DefWindowProcA(window, msg, wparam, lparam),
     }
 }
 
